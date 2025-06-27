30526d4de7c128d98665306a6bf66b6cfae9324c
Ashutosh Jha <166474047+AJpacific@users.noreply.github.com>
2025-06-27 15:38:18 +0530

    Update index.js

--- Backend Test Submission/src/index.js
+++ Backend Test Submission/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const shortUrlRoutes = require('./routes/shorturls');
-const loggerMiddleware = require('../../LoggingMiddleware/logger');
+const loggerMiddleware = require('../../Logging Middleware/logger');
 const { connectDB } = require('./config/db');
 
 dotenv.config({ path: './.env' });
